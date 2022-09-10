62d739eb951e9cc1ecdd55038835573834b5f0be
Jacky Zhao <j.zhao2k19@gmail.com>
2022-09-09 18:24:48 -0700

    reimplement using splayset

--- src/crdt.rs
+++ src/crdt.rs
@@ -1,71 +1,187 @@
-use std::{cmp::Ordering};
-use crate::linkedlist::{Ref, LinkedList};
+use splay_tree::SplaySet;
+use std::cmp::max;
+use std::cmp::Ordering;
+use std::rc::Weak;
 
-type UUID = i64;
-struct InsertOp<T> {
-    id: UUID,
-    origin: Ref<InsertOp<T>>, 
+type AuthorID = u64;
+type SequenceNumber = u64;
+type OpID = (AuthorID, SequenceNumber);
+
+pub struct Element<T> {
+    id: OpID,
+    origin: Option<Weak<Element<T>>>,
     is_deleted: bool,
-    content: T,
-    sequence_number: usize,
+    content: Option<T>,
 }
 
-impl<T> InsertOp<T> {
-    fn parent_ref(&self) -> &InsertOp<T> {
-        unsafe { (*self.origin.as_ptr()).elem() }
-    } 
+impl<T> Element<T> {
+    pub fn author(&self) -> AuthorID {
+        self.id.0
+    }
+
+    pub fn sequence_num(&self) -> SequenceNumber {
+        self.id.1
+    }
+
+    pub fn ghost_element(with_id: OpID) -> Element<T> {
+        Element {
+            id: with_id,
+            origin: None,
+            is_deleted: false,
+            content: None,
+        }
+    }
+
+    pub fn origin_deref(&self) -> Option<&Element<T>> {
+        self.origin
+            .as_ref()
+            .map(|origin| unsafe { &*origin.as_ptr() })
+    }
+}
+const ROOT_ID: OpID = (0, 0);
+
+pub struct ListCRDT<T> {
+    our_id: AuthorID,
+    splayset: SplaySet<Element<T>>,
+    highest_sequence_number: SequenceNumber,
+    size: usize,
 }
 
-impl<T> PartialEq for InsertOp<T> {
+impl<T> ListCRDT<T>
+where
+    T: Eq,
+{
+    pub fn new(id: AuthorID) -> ListCRDT<T> {
+        let mut splayset = SplaySet::new();
+        splayset.insert(Element {
+            id: ROOT_ID,
+            origin: None,
+            is_deleted: false,
+            content: None,
+        });
+        ListCRDT {
+            our_id: id,
+            splayset,
+            highest_sequence_number: 0,
+            size: 0,
+        }
+    }
+
+    fn find_by_id(&mut self, id: OpID) -> Option<Weak<Element<T>>> {
+        // construct a fake element so we can query by id
+        let fake_element = Element::ghost_element(id);
+        self.splayset.get(&fake_element).map(|elt| {
+            let node: *const Element<T> = elt;
+            unsafe { Weak::from_raw(node) }
+        })
+    }
+
+    pub fn insert(&mut self, after: OpID, content: T) -> OpID {
+        let id = (self.our_id, self.highest_sequence_number + 1);
+        let origin = self.find_by_id(after);
+        self.insert_remote(Element {
+            id,
+            origin,
+            is_deleted: false,
+            content: Some(content),
+        });
+        id
+    }
+
+    pub fn delete(&mut self, id: OpID) {
+        self.insert_remote(Element {
+            id,
+            origin: None,
+            is_deleted: true,
+            content: None,
+        })
+    }
+
+    pub fn insert_remote(&mut self, elt: Element<T>) {
+        let elt_seq_num = elt.sequence_num();
+        let elt_is_deleted = elt.is_deleted;
+        println!("insert -> {:?}", elt.id);
+        self.splayset.insert(elt);
+        self.highest_sequence_number = max(elt_seq_num, self.highest_sequence_number);
+        if !elt_is_deleted {
+            self.size += 1;
+        }
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            inner_iter: self.splayset.iter(),
+        }
+    }
+}
+
+pub struct Iter<'a, T> {
+    inner_iter: splay_tree::set::Iter<'a, Element<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(elt) = self.inner_iter.next() {
+            if elt.id != ROOT_ID && !elt.is_deleted && elt.content.is_some() {
+                return elt.content.as_ref();
+            }
+        }
+        None
+    }
+}
+
+impl<T> PartialEq for Element<T> {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
 }
 
-impl<T> Eq for InsertOp<T> {}
+impl<T> Eq for Element<T> {}
 
-struct CRDT<T> {
-    ops: LinkedList<InsertOp<T>>,
-    highest_sequence_number: usize,
-}
-
-impl<T> CRDT<T> {
-    fn new() -> Self {
-        Self {
-            ops: LinkedList::new(),
-            highest_sequence_number: 0,
-        }
-    }
-
-    fn insert(&mut self, new_op: InsertOp<T>) {
-        // seek the cursor to where the parent is
-        let mut insert_cursor = self.ops.cursor();
-        insert_cursor.seek_forward_until(new_op.parent_ref());
-
-        while let Some(cur_op) = insert_cursor.peek() {
-            if new_op.sequence_number > cur_op.sequence_number {
-                // new op is larger sequence number, safe to insert
-                break;
-            }
-
-            // check our current insert's parent
-            let mut cur_parent_cursor = self.ops.cursor();
-            cur_parent_cursor.seek_forward_until(cur_op.parent_ref());
-
-            // YATA rule 1: make sure that origin lines never cross
-            let origin_line_crossing = cur_parent_cursor.index() < insert_cursor.index();
-            // YATA rule 3: tie break by UUID, insertion with smaller id is on left
-            let tie = cur_parent_cursor.index() == insert_cursor.index();
-            let smaller_uuid = new_op.sequence_number == cur_op.sequence_number && new_op.id < cur_op.id;
-            if origin_line_crossing || (tie && smaller_uuid) {
-                break;
-            }
-
-            insert_cursor.seek_forward();
+impl<T> Ord for Element<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // if we have a bigger sequence number, we are bigger
+        if self.sequence_num() > other.sequence_num() {
+            return Ordering::Greater;
         }
 
-        let mut c = self.ops.mut_cursor_from_ref_idx(insert_cursor.raw_ref(), insert_cursor.index());
-        c.push_after(new_op);
+        // if index of our parent is > than the index of other parent, we are bigger
+        let our_origin = self.origin_deref();
+        let other_origin = other.origin_deref();
+        match our_origin.cmp(&other_origin) {
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Less => Ordering::Less,
+            Ordering::Equal => {
+                // parents are equal, is sequence number the same?
+                if self.sequence_num() == other.sequence_num() {
+                    // tie break on author id
+                    self.author().cmp(&other.author())
+                } else {
+                    // if sequence number is not > or == then it must be <
+                    Ordering::Less
+                }
+            }
+        }
     }
 }
 
+impl<T> PartialOrd for Element<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{ListCRDT, ROOT_ID};
+
+    #[test]
+    fn test_basic_doc() {
+        let mut doc = ListCRDT::<u16>::new(0);
+        let first_op = doc.insert(ROOT_ID, 0);
+        let second_op = doc.insert(first_op, 1);
+        let third_op = doc.insert(ROOT_ID, 2);
+        assert_eq!(doc.iter().collect::<Vec<_>>(), vec![&2, &0, &1]);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
-pub mod linkedlist;
 pub mod crdt;
