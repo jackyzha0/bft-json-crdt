41fee2b88a9c2f4727ff03e7dcfdb8b1f3d71765
Jacky Zhao <j.zhao2k19@gmail.com>
2022-10-12 20:33:12 -0700

    move editing trace to e2e tests, add readme and benchmarks

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-## Resources
-1. Paper: https://martin.kleppmann.com/2022/04/05/bft-crdt-papoc.html 
-2. Rust to WASM: https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm#using_the_package_on_the_web
-3. Shrinking WASM Size: https://rustwasm.github.io/book/game-of-life/code-size.html#shrinking-wasm-size
+# Byzantine Fault Tolerant CRDTs
+
+This work is mainly inspired by implementing Martin Kleppmann's 2022 paper on *Making CRDTs Byzantine Fault Tolerant* ([source](https://martin.kleppmann.com/papers/bft-crdt-papoc22.pdf))
+on top of a simplified [Automerge](https://automerge.org/) implementation.
+
+The goal is to show a working prototype that demonstrated in simple code the ideas behind
+1. An Automerge-like CRDT
+2. How a primitive list CRDT can be composed to create complex CRDTs like JSON
+2. How to add Byzantine Fault Tolerance to arbitrary CRDTs
+
+Unlike most other CRDT implementations, I leave out many performance optimizations that would make the basic algorithm harder to understand.
+
+## Benchmarks
+Altough this implementation does not optimize for performance, it still nonetheless performs quite well.
+
+Benchmarking happened on a 2019 Macbook Pro with a 2.6GHz i7.
+Numbers are compared to  which report their performance benchmarks [here](https://github.com/automerge/automerge-perf)
+
+| # Ops | Raw String (JS) | Ours (basic) | Ours (BFT) | Automerge |
+|--|--|--|--|
+|10k       | -     | 0.085s  | - | 1.6s    |
+|100k      | -     | 11.321s | - | 43.0s   |
+|All (259k)| 0.61s | 110.040s| - | Timeout |
---
+++ tests/kleppmann_trace.rs
@@ -0,0 +1,73 @@
+use bft_json_crdt::list_crdt::ListCRDT;
+use bft_json_crdt::op::{OpID, ROOT_ID};
+use std::{fs::File, io::Read};
+use time::PreciseTime;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Edit {
+    pos: usize,
+    delete: bool,
+    #[serde(default)]
+    content: Option<char>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Trace {
+    final_text: String,
+    edits: Vec<Edit>,
+}
+
+fn get_trace() -> Trace {
+    let fp = "./tests/edits.json";
+    match File::open(fp) {
+        Err(e) => panic!("Open edits.json failed: {:?}", e.kind()),
+        Ok(mut file) => {
+            let mut content: String = String::new();
+            file.read_to_string(&mut content)
+                .expect("Problem reading file");
+            serde_json::from_str(&content).expect("JSON was not well-formatted")
+        }
+    }
+}
+
+#[test]
+fn test_editing_trace() {
+    let t = get_trace();
+    let mut list = ListCRDT::new(1);
+    let mut ops: Vec<OpID> = Vec::new();
+    ops.push(ROOT_ID);
+    let start = PreciseTime::now();
+    let edits = t.edits.to_vec();
+    for (i, op) in edits.iter().enumerate() {
+        let origin = ops[op.pos];
+        if op.delete {
+            let delete_op = list.delete(origin);
+            ops.push(delete_op.id);
+        } else {
+            let new_op = list.insert(origin, op.content.unwrap());
+            ops.push(new_op.id);
+        }
+
+        match i {
+            10_000 | 100_000 => {
+                let end = PreciseTime::now();
+                let runtime_sec = start.to(end);
+                println!("took {:?} to run {i} ops", runtime_sec);
+            }
+            _ => {}
+        };
+    }
+
+    let end = PreciseTime::now();
+    let runtime_sec = start.to(end);
+    println!("took {:?} to finish", runtime_sec);
+    let result = list.iter().collect::<String>();
+    let expected = t.final_text;
+    assert_eq!(result.len(), expected.len());
+    assert_eq!(result, expected);
+    assert!(false);
+}
